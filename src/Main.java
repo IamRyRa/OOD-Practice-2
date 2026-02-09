ced60e70c6a986caf9b98777508ed7e0e94702e4
IamRyRa <iamreyhanerki@gmail.com>
2026-02-09 12:01:34 -0800

    edit main

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,11 @@ import edu.ticket.TicketService;
 public class Main {
 
     public static void main(String[] args) {
-
         TicketService ticketService = new TicketService();
-
-        // Example 1: Bug reported from web
-        Ticket ticket = new Ticket(1,"WEB", "BUG");
+        TicketFactory factory = new WebTicketFactory();
+        Ticket ticket = factory.createTicket(1, "BUG");
         ticket.setRequest("I see a very very BAD BUG!");
+        
         ticketService.handle(ticket);
     }
 }
